bbfe8a0468d2e78447e1db376b1722c61b318131
salboaie <salboaie@gmail.com>
2022-03-03 13:03:58 +0200

    added support for performance testing

---
+++ tests/perfromance/assertPerfromanceWithErrors.js
@@ -0,0 +1,18 @@
+
+const assert = require("../../lib/checksCore").assert;
+
+let TIMEOUT = 3000;
+
+let cfg= {
+        timeOut: TIMEOUT,
+        minRatePerSecond:100,
+        testFunction: function (end){
+            end(new Error("Fake error"));
+        }
+};
+
+assert.begin("Performance testing", TIMEOUT + 1000);
+
+assert.performance(cfg, (errs, result) => {
+    console.log("Executed successfully ", result.actualRate, " steps per second with ", errs.length, " errors, like for example:" + errs[0]);
+});
